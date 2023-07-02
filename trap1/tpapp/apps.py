f6592c22a8a262afd54a30a3eee99f97678738f6
Vakkut <136303471+Vakkut@users.noreply.github.com>
2023-07-03 01:50:03 +0530

    Delete trap1 directory

--- trap1/tpapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tpapp'
--- trap1/tpapp/urls.py
+++
@@ -1,9 +0,0 @@
-from .import views
-from django.urls import path,include
-
-urlpatterns = [
-
-    path('',views.demo,name='demo')
-    # ,path('add/',views.add,name='addition')
-
-]
